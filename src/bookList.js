c9cd22ec7fa761041b8edccc7505ea1ff5acf9df
Oct1 <591102252@qq.com>
2019-01-15 22:32:17 +0800

    add search

--- src/bookList.js
+++ src/bookList.js
@@ -15,11 +15,11 @@ class ListBook extends React.Component {
         };
         render() {
                 return ( 
-                    <div className = "list-books" >
-                        <div className = "list-books-title" >
-                            <h1 > MyReads </h1> 
-                        </div > 
-                        <div className = "list-books-content" >
+                    <div className = "list-books">
+                        <div className = "list-books-title">
+                            <h1> MyReads </h1> 
+                        </div> 
+                        <div className = "list-books-content">
                             <div> {
                                 this.state.bookShelves.map(shelf =>
                                     <Component 
